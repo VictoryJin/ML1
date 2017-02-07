cd370511b3b312d9fdfb298e14fe7b7d14084763
진유림 <jin-yoolim@jin-yulim-ui-MacBook-Pro.local>
2017-02-07 02:12:59 -0800

    cleaner file finished

--- cleaner.py
+++ cleaner.py
@@ -1,4 +1,63 @@
 import pandas as pd
+import numpy as np
+from nltk import SnowballStemmer
+from nltk.corpus import stopwords
+import string as st
+import re
+
+hrc_train = pd.read_csv("./data/HRC_train.tsv", sep = "\t", names = ["id", "text"])
 
 
-pd.read_csv("cleaned_hrc_train_text.tsv", sep = "\t")
+badstrings = ["unclassified u.s. department of state", "case no. ............",
+                "doc no. c........", "date: ..........","state dept. . produced to house select benghazi comm.",
+                "subject to agreement on sensitive information & redactions.","no foia waiver state...........",
+                "no foia waiver.",  "unclassified us department of state"]
+
+snow = SnowballStemmer('english')
+stop = set(stopwords.words('english'))
+
+#spits out a 'cleaned' text indexed after the location of a string, if there's any.
+def truncStrings(text, string, index):
+    try:
+        j = text.index(str(string))
+        newstrings = text[j + len(string):]
+        return truncStrings(newstrings, string, j)
+    except ValueError:
+        # print("the " + string + "was found in index number: " + str(index))
+        return text
+
+#unifies stemmed words and stop words within the string
+def stopstem(string):
+    #tokenize the words
+    words = string.split()
+    #removes stop words
+    wordlist = [i for i in words if i not in stop]
+    #unifies stemmed words
+    wordlist = [snow.stem(i) for i in wordlist]
+    return ' '.join(wordlist)
+
+#removes unnecessary punctuation
+def rmpunctuation(string):
+    string = re.sub(r'\\', "", string)
+    string = re.sub(r'-', ' ', string)
+    return ' '.join(word.strip(st.punctuation) for word in string.split())
+
+#cleans the data
+def clean(hrc_data, remove):
+    hrc_copy = hrc_data.copy()
+    for i in range(len(hrc_copy)):
+        #truncates string before "subjects:", "sent:", "re:", and "fw:" to display only the latest content
+        newtext = truncStrings(hrc_copy.loc[i, "text"], "subject:", 0)
+        newtext = truncStrings(newtext, "sent:", 0)
+        newtext = truncStrings(newtext, "re:", 0)
+        newtext = truncStrings(newtext, "fw:", 0)
+        for bad in badstrings:
+            newtext = re.sub(bad, "", newtext)
+        #removes stop words and unifies stemmed words
+        newtext = stopstem(newtext)
+        newtext = rmpunctuation(newtext)
+        hrc_copy.loc[i, "text"] = newtext
+    return hrc_copy
+
+
+new = clean(hrc_train, badstrings)
---
+++ data_explorers.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+
+hrc_train = pd.read_csv("./data/HRC_train.tsv", sep = "\t", names = ["id", "text"])
+len(hrc_train)
+
+#removes bad strings within a text
+def removeBadStrings(oldstrings, rownumber):
+    try:
+        j = oldstrings.index("sent:")
+        newstrings = oldstrings[j+5:]
+        return removeBadStrings(newstrings, rownumber)
+    except ValueError:
+        return oldstrings
+
+# print(hrc_train.loc[0, 'text'])
+removeBadStrings(hrc_train.loc[0, 'text'], 0)
+
+#identifies the index of a specific string within a text
+def identify(text, string, index):
+    try:
+        j = text.index(str(string))
+        newstrings = text[j + len(string):]
+        return identify(newstrings, string, j)
+    except ValueError:
+        # print("the " + string + "was found in index number: " + str(index))
+        return index
+
+ind = np.zeros(len(hrc_train))
+for i in range(len(hrc_train)):
+    ind[i] = identify(hrc_train.loc[i, "text"], "subject:", 0)
+print(len([i for i, j in enumerate(ind) if j == 0]))
+# TODO: test hrc_train cleaning method
