c89598b2a058ab8c11a9bedc256439ce9a22bc03
진유림 <jin-yoolim@jin-yulim-ui-MacBook-Pro.local>
2017-02-08 16:29:08 -0800

    simplified naive bayesian

--- NBpred.py
+++ NBpred.py
@@ -19,7 +19,6 @@ y_test, X_test = email_test.id, email_test.txt
 X_train_dtm = vect.fit_transform(X_train.values.astype('U'))
 X_test_dtm = vect.transform(X_test.values.astype('U'))
 #fit and predict wrt NB
-nb.fit(X_train_dtm, y_train)
-y_pred_class = nb.predict(X_test_dtm)
+y_pred_class = nb.fit(X_train_dtm, y_train).predict(X_test_dtm)
 print("accuracy was ", metrics.accuracy_score(y_test, y_pred_class))
 print(metrics.confusion_matrix(y_test, y_pred_class))
