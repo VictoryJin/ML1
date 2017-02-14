1cb166f1bcda6bdfb427df4a7cd2ec7c21a55030
진유림 <jin-yoolim@jin-yulim-ui-MacBook-Pro.local>
2017-02-13 17:36:01 -0800

    reverted parameters

--- SVMpred.py
+++ SVMpred.py
@@ -18,5 +18,7 @@ y_test, X_test = email_test.id, email_test.txt
 X_train_dtm = v.fit_transform(X_train.values.astype('U'))
 X_test_dtm = v.transform(X_test.values.astype('U'))
 
+
+#print accuracy
 y_pred_class = clf.fit(X_train_dtm, y_train).predict(X_test_dtm)
 print("accuracy was ", metrics.accuracy_score(y_test, y_pred_class))
