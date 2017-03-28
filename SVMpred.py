68a4a7df9ff23071c86bfe701a6036f53d9809cc
진유림 <jin-yoolim@jin-yulim-ui-MacBook-Pro.local>
2017-03-28 14:56:35 +0900

    boosted accuracy to 82%

--- SVMpred.py
+++ SVMpred.py
@@ -6,7 +6,7 @@ from sklearn import model_selection
 import matplotlib.pyplot as plt
 
 
-v = CountVectorizer()
+v = CountVectorizer(ngram_range=(1, 2), stop_words="english")
 
 #retrives the Cross-validation value for data
 def getCV(estimator, data, labels):
@@ -17,27 +17,27 @@ def getSVMEst(c):
     return svm.LinearSVC(C = c)
 
 #retrives the best margin value 'c' for the data, and plot the data
-def getBestSVMParam(data, labels, cmin = 200, cmax = 300):
-    minCV = float('inf')
-    minParam = 0
-    costs = []
-    cvs = []
-    for c in np.arange(cmin, cmax, 4):
-        svms = getSVMEst(c)
-        cv = getCV(svms, data, labels)
-        if cv < minCV:
-            minCV = cv
-            minParam = c
-        costs.append(c)
-        cvs.append(cv)
-    plt.plot(costs, cvs)
-    plt.xlabel("costs")
-    plt.ylabel("cross-validation scores")
-    plt.show()
-    return minParam
+# def getBestSVMParam(data, labels, cmin = 0.01, cmax = 300):
+#     minCV = float('inf')
+#     minParam = 0
+#     costs = []
+#     cvs = []
+#     for c in np.arange(cmin, cmax, 0.1):
+#         svms = getSVMEst(c)
+#         cv = getCV(svms, data, labels)
+#         if cv < minCV:
+#             minCV = cv
+#             minParam = c
+#         costs.append(c)
+#         cvs.append(cv)
+#     plt.plot(costs, cvs)
+#     plt.xlabel("costs")
+#     plt.ylabel("cross-validation scores")
+#     plt.show()
+#     return minParam
 
 # set path
-train_path, test_path = "data/HRC_train_cleaned.tsv", "data/HRC_test_cleaned.tsv"
+train_path, test_path = "data/HRC_train.tsv", "data/HRC_test.tsv"
 # import train data & set variables
 email_train = pd.read_table(train_path, header=None, names=['id', 'txt'], sep='\t')
 y_train, X_train = email_train.id, email_train.txt
@@ -51,10 +51,7 @@ label = email_train['id'].values
 X_train_dtm = v.fit_transform(X_train.values.astype('U'))
 X_test_dtm = v.transform(X_test.values.astype('U'))
 
-bestparam = getBestSVMParam(X_train_dtm, label)
-bestparam = 270
-
-
+# bestparam = getBestSVMParam(X_train_dtm, label)
 
 #print accuracy
 clf = svm.LinearSVC()
